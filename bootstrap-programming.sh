0e0d4c0d27a777ca51a6400fd0bb19911a231622
jghoover <30665181+jghoover@users.noreply.github.com>
2018-05-10 14:01:43 -0500

    Update bootstrap-programming.sh

--- bootstrap-programming.sh
+++ bootstrap-programming.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-apt-get install -y python gcc gfortran sbcl build-essential
+apt-get install -y python gcc gfortran sbcl build-essential libopenmpi-dev openmpi-bin
 mkdir python
 mkdir cpp
 mkdir fortran
