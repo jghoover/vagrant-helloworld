1cdce2dbae4ebf271afd1a42b8c8afb85c6c0d21
jghoover <30665181+jghoover@users.noreply.github.com>
2018-05-10 13:23:24 -0500

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 apt-get update
-apt-get install -y apache2 git
+apt-get install -y apache2 git emacs
 if ! [ -L /var/www ]; then
   rm -rf /var/www
   ln -fs /vagrant /var/www
